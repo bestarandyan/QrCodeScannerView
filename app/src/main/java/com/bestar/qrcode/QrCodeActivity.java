2f411a1c9e87bd4b7b2e6534f709c35afc7625f1
刘星星 <bestar@lifang.com>
2018-03-05 17:42:43 +0800

    Initial commit

---
+++ app/src/main/java/com/bestar/qrcode/QrCodeActivity.java
@@ -0,0 +1,66 @@
+package com.bestar.qrcode;
+
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.bestar.qrcode.qrcode.OnScannerCompletionListener;
+import com.bestar.qrcode.qrcode.ScannerView;
+import com.google.zxing.Result;
+import com.google.zxing.client.result.ParsedResult;
+
+public class QrCodeActivity extends AppCompatActivity implements View.OnClickListener, OnScannerCompletionListener {
+    private ScannerView mScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+        mScannerView = findViewById(R.id.scanner_view);
+        mScannerView.setOnScannerCompletionListener(this);
+        mScannerView.setLaserFrameBoundColor(Color.WHITE);
+        mScannerView.setLaserGridLineResId(R.mipmap.zfb_grid_scan_line);
+        mScannerView.setLaserFrameCornerLength(30);
+        mScannerView.setLaserFrameCornerWidth(3);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        onResumeScannerView();
+    }
+
+    @Override
+    public void onPause() {
+        mScannerView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void OnScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode) {
+
+    }
+
+    private void onResumeScannerView() {
+        try {
+            mScannerView.onResume();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Toast.makeText(this, "调用相机异常，请检查是否授权！", Toast.LENGTH_SHORT).show();
+            this.finish();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/bestar/qrcode/qrcode/result/Result.java
@@ -0,0 +1,9 @@
+package com.bestar.qrcode.qrcode.result;
+
+import java.io.Serializable;
+
+/**
+ * Created by hupei on 2016/8/12.
+ */
+class Result implements Serializable {
+}
